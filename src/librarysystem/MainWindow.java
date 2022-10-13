4fd484bfcb7770bb40ad8c113e8d11e38b126a5c
Tewodroes <teddymeselu@gmail.com>
2022-10-13 17:45:35 -0500

    Main Window final UI

--- src/librarysystem/MainWindow.java
+++ src/librarysystem/MainWindow.java
@@ -16,6 +16,15 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 import java.awt.Cursor;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+import javax.swing.UIManager;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
 
 
 /**
@@ -27,6 +36,24 @@ import java.awt.Cursor;
 public class MainWindow extends JFrame{	
 	private JPanel contentPane;
 	JLayeredPane layeredPane;
+	private JTextField textMemberID;
+	private JTextField textISBN;
+	private JTextField textFirstName;
+	private JTextField textLastName;
+	private JTextField textTelephone;
+	private JTextField textCity;
+	private JTextField textState;
+	private JTextField textZip;
+	private JTextField textAddCopyISBN;
+	private JTextField textbNumOfCopy;
+	private JTextField textCheckoutRecordMemberID;
+	private JTable tableRecord;
+	private JTextField textAddBookISBN;
+	private JTextField textTitle;
+	private JTextField textCheckoutPeriod;
+	private JTextField textFNumberOfCopies;
+	private JTextField textOverdueISBN;
+	private JTable tableOverdue;
 
 	public static void main(String[] args) {
 		
@@ -55,58 +82,376 @@ public class MainWindow extends JFrame{
 		contentPane.setLayout(null);
 		
 		layeredPane = new JLayeredPane();
-		layeredPane.setBounds(10, 79, 850, 550);
+		layeredPane.setAlignmentY(0.0f);
+		layeredPane.setAlignmentX(0.0f);
+		layeredPane.setBackground(new Color(0, 128, 128));
+		layeredPane.setBounds(0, 50, 850, 550);
 		contentPane.add(layeredPane);
 		layeredPane.setLayout(new CardLayout(0, 0));
 		
 		JPanel panelCheckoutBook = new JPanel();
+		panelCheckoutBook.setBackground(new Color(64, 128, 128));
 		layeredPane.add(panelCheckoutBook, "name_176444819990800");
+		panelCheckoutBook.setLayout(null);
 		
-		JLabel lblCheckOutBook = new JLabel("Checkout Book");
+		JLabel lblCheckOutBook = new JLabel("CHECKOUT BOOK");
+		lblCheckOutBook.setFont(new Font("Tahoma", Font.BOLD, 26));
+		lblCheckOutBook.setForeground(new Color(255, 255, 255));
+		lblCheckOutBook.setBounds(286, 44, 259, 46);
 		lblCheckOutBook.setHorizontalAlignment(SwingConstants.CENTER);
 		panelCheckoutBook.add(lblCheckOutBook);
 		
-		JPanel panelAddMember = new JPanel();
-		layeredPane.add(panelAddMember, "name_176496424079300");
+		textMemberID = new JTextField();
+		textMemberID.setBounds(299, 149, 246, 33);
+		panelCheckoutBook.add(textMemberID);
+		textMemberID.setColumns(10);
 		
-		JLabel lblAddMember = new JLabel("Add Member");
+		JLabel lblMemberId = new JLabel("MEMBER ID");
+		lblMemberId.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblMemberId.setForeground(new Color(255, 255, 255));
+		lblMemberId.setAutoscrolls(true);
+		lblMemberId.setBounds(152, 147, 121, 33);
+		panelCheckoutBook.add(lblMemberId);
+		
+		textISBN = new JTextField();
+		textISBN.setColumns(10);
+		textISBN.setBounds(299, 222, 246, 33);
+		panelCheckoutBook.add(textISBN);
+		
+		JLabel lblISBN = new JLabel("ISBN");
+		lblISBN.setForeground(Color.WHITE);
+		lblISBN.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblISBN.setAutoscrolls(true);
+		lblISBN.setBounds(152, 222, 121, 33);
+		panelCheckoutBook.add(lblISBN);
+		
+		JButton btnCheckout = new JButton("CHECKOUT");
+		btnCheckout.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnCheckout.setBorder(null);
+		btnCheckout.setForeground(Color.WHITE);
+		btnCheckout.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnCheckout.setBackground(new Color(0, 64, 64));
+		btnCheckout.setBounds(344, 313, 157, 53);
+		panelCheckoutBook.add(btnCheckout);
+		
+		JPanel panelAddMember = new JPanel();
+		panelAddMember.setBackground(new Color(64, 128, 128));
+		layeredPane.add(panelAddMember, "name_176496424079300");
+		panelAddMember.setLayout(null);
+		
+		JLabel lblAddMember = new JLabel("ADD NEW MEMBER");
+		lblAddMember.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAddMember.setForeground(Color.WHITE);
+		lblAddMember.setFont(new Font("Tahoma", Font.BOLD, 26));
+		lblAddMember.setBounds(255, 21, 259, 46);
 		panelAddMember.add(lblAddMember);
 		
-		JPanel panelAddBookCopy = new JPanel();
-		layeredPane.add(panelAddBookCopy, "name_176498428824900");
+		JLabel lblFirstName = new JLabel("FIRST NAME");
+		lblFirstName.setForeground(Color.WHITE);
+		lblFirstName.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblFirstName.setAutoscrolls(true);
+		lblFirstName.setBounds(148, 113, 121, 33);
+		panelAddMember.add(lblFirstName);
 		
-		JLabel lblAddBookCopy = new JLabel("Add Book Copy");
-		panelAddBookCopy.add(lblAddBookCopy);
+		textFirstName = new JTextField();
+		textFirstName.setColumns(10);
+		textFirstName.setBounds(312, 113, 246, 33);
+		panelAddMember.add(textFirstName);
+		
+		JLabel lblLastName = new JLabel("LAST NAME");
+		lblLastName.setForeground(Color.WHITE);
+		lblLastName.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblLastName.setAutoscrolls(true);
+		lblLastName.setBounds(148, 157, 121, 33);
+		panelAddMember.add(lblLastName);
+		
+		textLastName = new JTextField();
+		textLastName.setColumns(10);
+		textLastName.setBounds(312, 157, 246, 33);
+		panelAddMember.add(textLastName);
+		
+		JLabel lblTelephone = new JLabel("TELEPHONE");
+		lblTelephone.setForeground(Color.WHITE);
+		lblTelephone.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblTelephone.setAutoscrolls(true);
+		lblTelephone.setBounds(148, 201, 121, 33);
+		panelAddMember.add(lblTelephone);
+		
+		textTelephone = new JTextField();
+		textTelephone.setColumns(10);
+		textTelephone.setBounds(312, 201, 246, 33);
+		panelAddMember.add(textTelephone);
+		
+		JLabel lblCity = new JLabel("CITY");
+		lblCity.setForeground(Color.WHITE);
+		lblCity.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblCity.setAutoscrolls(true);
+		lblCity.setBounds(148, 245, 121, 33);
+		panelAddMember.add(lblCity);
+		
+		textCity = new JTextField();
+		textCity.setColumns(10);
+		textCity.setBounds(312, 245, 246, 33);
+		panelAddMember.add(textCity);
+		
+		JLabel lblState = new JLabel("STATE");
+		lblState.setForeground(Color.WHITE);
+		lblState.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblState.setAutoscrolls(true);
+		lblState.setBounds(148, 289, 121, 33);
+		panelAddMember.add(lblState);
+		
+		textState = new JTextField();
+		textState.setColumns(10);
+		textState.setBounds(312, 289, 246, 33);
+		panelAddMember.add(textState);
+		
+		JLabel lblZiP = new JLabel("ZIP");
+		lblZiP.setForeground(Color.WHITE);
+		lblZiP.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblZiP.setAutoscrolls(true);
+		lblZiP.setBounds(148, 333, 121, 33);
+		panelAddMember.add(lblZiP);
+		
+		textZip = new JTextField();
+		textZip.setColumns(10);
+		textZip.setBounds(312, 333, 246, 33);
+		panelAddMember.add(textZip);
+		
+		JButton btnAdd = new JButton("ADD");
+		btnAdd.setForeground(Color.WHITE);
+		btnAdd.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnAdd.setBorder(null);
+		btnAdd.setBackground(new Color(0, 64, 64));
+		btnAdd.setBounds(357, 398, 157, 53);
+		panelAddMember.add(btnAdd);
+		
+		JPanel panelAddBookCopy = new JPanel();
+		panelAddBookCopy.setBackground(new Color(64, 128, 128));
+		layeredPane.add(panelAddBookCopy, "name_176498428824900");
+		panelAddBookCopy.setLayout(null);
+		
+		JLabel lblAddMember_1 = new JLabel("ADD NEW MEMBER");
+		lblAddMember_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAddMember_1.setForeground(Color.WHITE);
+		lblAddMember_1.setFont(new Font("Tahoma", Font.BOLD, 26));
+		lblAddMember_1.setBounds(292, 76, 259, 46);
+		panelAddBookCopy.add(lblAddMember_1);
+		
+		JLabel lblIsbn = new JLabel("ISBN");
+		lblIsbn.setForeground(Color.WHITE);
+		lblIsbn.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblIsbn.setAutoscrolls(true);
+		lblIsbn.setBounds(154, 166, 121, 33);
+		panelAddBookCopy.add(lblIsbn);
+		
+		textAddCopyISBN = new JTextField();
+		textAddCopyISBN.setColumns(10);
+		textAddCopyISBN.setBounds(349, 168, 246, 33);
+		panelAddBookCopy.add(textAddCopyISBN);
+		
+		JLabel lblNumberOfCopy = new JLabel("NUMBER OF COPY");
+		lblNumberOfCopy.setForeground(Color.WHITE);
+		lblNumberOfCopy.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNumberOfCopy.setAutoscrolls(true);
+		lblNumberOfCopy.setBounds(154, 212, 152, 33);
+		panelAddBookCopy.add(lblNumberOfCopy);
+		
+		textbNumOfCopy = new JTextField();
+		textbNumOfCopy.setColumns(10);
+		textbNumOfCopy.setBounds(349, 212, 246, 33);
+		panelAddBookCopy.add(textbNumOfCopy);
+		
+		JButton btnAddCopy = new JButton("ADD COPY");
+		btnAddCopy.setForeground(Color.WHITE);
+		btnAddCopy.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnAddCopy.setBorder(null);
+		btnAddCopy.setBackground(new Color(0, 64, 64));
+		btnAddCopy.setBounds(408, 297, 157, 53);
+		panelAddBookCopy.add(btnAddCopy);
 		
 		JPanel panelAllBookIds = new JPanel();
+		panelAllBookIds.setBackground(new Color(64, 128, 128));
 		layeredPane.add(panelAllBookIds, "name_176499964787600");
+		panelAllBookIds.setLayout(null);
 		
-		JLabel lblAllBookIds = new JLabel("All Book Ids");
+		JLabel lblAllBookIds = new JLabel("ALL BOOK IDs");
+		lblAllBookIds.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAllBookIds.setForeground(Color.WHITE);
+		lblAllBookIds.setFont(new Font("Tahoma", Font.BOLD, 26));
+		lblAllBookIds.setBounds(272, 32, 227, 32);
 		panelAllBookIds.add(lblAllBookIds);
 		
-		JPanel panelAllMemberIds = new JPanel();
-		layeredPane.add(panelAllMemberIds, "name_178507496023200");
+		JScrollPane scrollPaneAllMemberID_1 = new JScrollPane();
+		scrollPaneAllMemberID_1.setBounds(59, 87, 718, 357);
+		panelAllBookIds.add(scrollPaneAllMemberID_1);
 		
-		JLabel lblAllMemberIds = new JLabel("All Member Ids");
+		JPanel panelAllMemberIds = new JPanel();
+		panelAllMemberIds.setBackground(new Color(64, 128, 128));
+		layeredPane.add(panelAllMemberIds, "name_178507496023200");
+		panelAllMemberIds.setLayout(null);
+		
+		JLabel lblAllMemberIds = new JLabel("ALL MEMBER IDs");
+		lblAllMemberIds.setBounds(269, 27, 227, 32);
+		lblAllMemberIds.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAllMemberIds.setForeground(Color.WHITE);
+		lblAllMemberIds.setFont(new Font("Tahoma", Font.BOLD, 26));
 		panelAllMemberIds.add(lblAllMemberIds);
 		
-		JPanel panelAddBook = new JPanel();
-		layeredPane.add(panelAddBook, "name_178636081207100");
+		JScrollPane scrollPaneAllBookID = new JScrollPane();
+		scrollPaneAllBookID.setBounds(56, 82, 718, 357);
+		panelAllMemberIds.add(scrollPaneAllBookID);
 		
-		JLabel lblAddBook = new JLabel("Add Book");
-		panelAddBook.add(lblAddBook);
+		JPanel panelAddBook = new JPanel();
+		panelAddBook.setBackground(new Color(64, 128, 128));
+		layeredPane.add(panelAddBook, "name_178636081207100");
+		panelAddBook.setLayout(null);
+		
+		JLabel lblAddNewBook = new JLabel("ADD NEW BOOK");
+		lblAddNewBook.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAddNewBook.setForeground(Color.WHITE);
+		lblAddNewBook.setFont(new Font("Tahoma", Font.BOLD, 26));
+		lblAddNewBook.setBounds(305, 11, 259, 46);
+		panelAddBook.add(lblAddNewBook);
+		
+		JLabel lblIsbn_1 = new JLabel("ISBN");
+		lblIsbn_1.setForeground(Color.WHITE);
+		lblIsbn_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblIsbn_1.setAutoscrolls(true);
+		lblIsbn_1.setBounds(142, 101, 121, 33);
+		panelAddBook.add(lblIsbn_1);
+		
+		textAddBookISBN = new JTextField();
+		textAddBookISBN.setColumns(10);
+		textAddBookISBN.setBounds(362, 103, 246, 33);
+		panelAddBook.add(textAddBookISBN);
+		
+		JLabel lblTitle = new JLabel("TITLE");
+		lblTitle.setForeground(Color.WHITE);
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblTitle.setAutoscrolls(true);
+		lblTitle.setBounds(142, 145, 121, 33);
+		panelAddBook.add(lblTitle);
+		
+		textTitle = new JTextField();
+		textTitle.setColumns(10);
+		textTitle.setBounds(362, 147, 246, 33);
+		panelAddBook.add(textTitle);
+		
+		JLabel lblCheckoutPeriod = new JLabel("CHECKOUT PERIOD");
+		lblCheckoutPeriod.setForeground(Color.WHITE);
+		lblCheckoutPeriod.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblCheckoutPeriod.setAutoscrolls(true);
+		lblCheckoutPeriod.setBounds(142, 189, 163, 33);
+		panelAddBook.add(lblCheckoutPeriod);
+		
+		textCheckoutPeriod = new JTextField();
+		textCheckoutPeriod.setColumns(10);
+		textCheckoutPeriod.setBounds(362, 191, 246, 33);
+		panelAddBook.add(textCheckoutPeriod);
+		
+		JLabel lblNumberOfCopies = new JLabel("NUMBER OF COPIES");
+		lblNumberOfCopies.setForeground(Color.WHITE);
+		lblNumberOfCopies.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNumberOfCopies.setAutoscrolls(true);
+		lblNumberOfCopies.setBounds(142, 233, 177, 33);
+		panelAddBook.add(lblNumberOfCopies);
+		
+		textFNumberOfCopies = new JTextField();
+		textFNumberOfCopies.setColumns(10);
+		textFNumberOfCopies.setBounds(362, 235, 246, 33);
+		panelAddBook.add(textFNumberOfCopies);
+		
+		JLabel lblAuthor = new JLabel("AUTHOR");
+		lblAuthor.setForeground(Color.WHITE);
+		lblAuthor.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblAuthor.setAutoscrolls(true);
+		lblAuthor.setBounds(142, 277, 121, 33);
+		panelAddBook.add(lblAuthor);
+		
+		JButton btnAddNewBook = new JButton("ADD");
+		btnAddNewBook.setForeground(Color.WHITE);
+		btnAddNewBook.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnAddNewBook.setBorder(null);
+		btnAddNewBook.setBackground(new Color(0, 64, 64));
+		btnAddNewBook.setBounds(405, 357, 157, 53);
+		panelAddBook.add(btnAddNewBook);
+		
+		JList listAuthor = new JList();
+		listAuthor.setBounds(362, 279, 246, 33);
+		panelAddBook.add(listAuthor);
 		
 		JPanel panelOverdue = new JPanel();
+		panelOverdue.setBackground(new Color(64, 128, 128));
 		layeredPane.add(panelOverdue, "name_178796255916100");
+		panelOverdue.setLayout(null);
 		
-		JLabel lblOverdue = new JLabel("Overdue");
-		panelOverdue.add(lblOverdue);
+		JLabel lblOverdueBooks = new JLabel("OVERDUE BOOKS");
+		lblOverdueBooks.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOverdueBooks.setForeground(Color.WHITE);
+		lblOverdueBooks.setFont(new Font("Tahoma", Font.BOLD, 26));
+		lblOverdueBooks.setBounds(251, 29, 273, 46);
+		panelOverdue.add(lblOverdueBooks);
+		
+		JLabel lblOverdueISBN = new JLabel("ISBN");
+		lblOverdueISBN.setForeground(Color.WHITE);
+		lblOverdueISBN.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblOverdueISBN.setAutoscrolls(true);
+		lblOverdueISBN.setBounds(32, 119, 121, 33);
+		panelOverdue.add(lblOverdueISBN);
+		
+		textOverdueISBN = new JTextField();
+		textOverdueISBN.setColumns(10);
+		textOverdueISBN.setBounds(163, 121, 233, 33);
+		panelOverdue.add(textOverdueISBN);
+		
+		JButton btnOverdueCheck = new JButton("CHECK");
+		btnOverdueCheck.setForeground(Color.WHITE);
+		btnOverdueCheck.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnOverdueCheck.setBorder(null);
+		btnOverdueCheck.setBackground(new Color(0, 64, 64));
+		btnOverdueCheck.setBounds(195, 191, 164, 53);
+		panelOverdue.add(btnOverdueCheck);
+		
+		tableOverdue = new JTable();
+		tableOverdue.setBounds(415, 155, 401, 269);
+		panelOverdue.add(tableOverdue);
 		
 		JPanel panelCheckoutRecord = new JPanel();
+		panelCheckoutRecord.setBackground(new Color(64, 128, 128));
 		layeredPane.add(panelCheckoutRecord, "name_179092286329700");
+		panelCheckoutRecord.setLayout(null);
 		
-		JLabel lblCheckoutRecord = new JLabel("Checkout Record");
+		JLabel lblCheckoutRecord = new JLabel("CHECKOUT RECORD");
+		lblCheckoutRecord.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCheckoutRecord.setForeground(Color.WHITE);
+		lblCheckoutRecord.setFont(new Font("Tahoma", Font.BOLD, 26));
+		lblCheckoutRecord.setBounds(245, 48, 273, 46);
 		panelCheckoutRecord.add(lblCheckoutRecord);
+		
+		JLabel lblCheckoutMemberID = new JLabel("MEMBER ID");
+		lblCheckoutMemberID.setForeground(Color.WHITE);
+		lblCheckoutMemberID.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblCheckoutMemberID.setAutoscrolls(true);
+		lblCheckoutMemberID.setBounds(26, 138, 121, 33);
+		panelCheckoutRecord.add(lblCheckoutMemberID);
+		
+		textCheckoutRecordMemberID = new JTextField();
+		textCheckoutRecordMemberID.setColumns(10);
+		textCheckoutRecordMemberID.setBounds(157, 140, 233, 33);
+		panelCheckoutRecord.add(textCheckoutRecordMemberID);
+		
+		JButton btnShowRecord = new JButton("SHOW RECORD");
+		btnShowRecord.setForeground(Color.WHITE);
+		btnShowRecord.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnShowRecord.setBorder(null);
+		btnShowRecord.setBackground(new Color(0, 64, 64));
+		btnShowRecord.setBounds(189, 210, 164, 53);
+		panelCheckoutRecord.add(btnShowRecord);
+		
+		tableRecord = new JTable();
+		tableRecord.setBounds(409, 174, 401, 269);
+		panelCheckoutRecord.add(tableRecord);
 		
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
